4c767550f4690a685e4a67caf5c61613df04084d
zhzhwcn <zhzhwmail@gmail.com>
2020-10-13 20:20:26 +0800

    添加项目文件。

---
+++ GithubPackageTest/Helper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubPackageTest
+{
+    public class Helper
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Hello From Github Package");
+        }
+    }
+}
