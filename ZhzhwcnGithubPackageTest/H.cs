ae3248c12863b6b9c5e68b498cdb2b5373cc3f1e
zhzhwcn <zhzhwmail@gmail.com>
2020-10-14 20:51:44 +0800

    add package

---
+++ ZhzhwcnGithubPackageTest/H.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZhzhwcnGithubPackageTest
+{
+    public class H
+    {
+        public static string Test()
+        {
+            return typeof(H).FullName;
+        }
+    }
+}
